8254d144816befab24e632d461088dd9271aa825
Renato <renatofss16@gmail.com>
2023-07-27 12:10:28 -0300

    codigo pronto para fazer o blog do inicio

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, post, page
 app_name = 'blog'
 urlpatterns = [
     path('', index, name='blog'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
